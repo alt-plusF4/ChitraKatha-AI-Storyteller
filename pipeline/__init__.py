03f82024d5b9b2e0272c7917a842d8f400f93507
alt-plusF4 <58546519+alt-plusF4@users.noreply.github.com>
2025-09-13 13:02:35 +0530

    Create __init__.py

---
+++ pipeline/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from .module0_casting import *
+from .module1_screenwriting import *
+from .module2_voiceover import *
+from .module3_image_generation import *
+from .module4_postproduction import *
+from .main_pipeline import create_story_video
